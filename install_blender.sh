9c093fdfa646eb3f8cd633bc73bc0936a32f3f0d
LeanderCretegny <leander.cretegny@epfl.ch>
2026-03-29 20:00:42 +0200

    update blender setup

---
+++ install_blender.sh
@@ -0,0 +1,8 @@
+#Test BLENDER va
+echo "${BLENDER}"
+
+#Install blender 4.5.8 LTS (possible upgrade: install latest 4.5 version)
+curl -L -O "https://download.blender.org/release/Blender4.5/${BLENDER}.tar.xz" 
+
+#unzip blender
+tar xf ${BLENDER}.tar.xz 
