ebe3588f8d7e3d43e64c26261d67b8e16983db75
LeanderCretegny <leander.cretegny@epfl.ch>
2026-03-29 19:20:32 +0200

    test installing blender in action

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-echo "hello actions not on main"
+#Defines utility var
+BLENDER_ARCHIVE=blender-4.5.8-linux-x64.tar.xz
+
+#Install blender 4.5.8 LTS (possible upgrade: install latest 4.5 version)
+curl -L -O "https://www.blender.org/download/lts/4-5/${BLENDER_ARCHIVE}" 
+
+#unzip blender
+tar xf ${BLENDER_ARCHIVE} --transform blender
+
+# Start blender
+./blender/blender -h
