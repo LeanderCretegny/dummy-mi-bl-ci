e88bfe7e85794ac3c450b4cfc9258bc277cc2b7a
LeanderCretegny <leander.cretegny@epfl.ch>
2026-03-29 20:11:42 +0200

    save

--- install_blender.sh
+++ install_blender.sh
@@ -1,6 +1,3 @@
-#Test BLENDER va
-echo "${BLENDER}"
-
 #Install blender 4.5.8 LTS (possible upgrade: install latest 4.5 version)
 curl -L -O "https://download.blender.org/release/Blender4.5/${BLENDER}.tar.xz" 
 
