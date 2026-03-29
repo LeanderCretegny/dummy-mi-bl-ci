4cdfd974f5771f35a2407995e7c7adf615654136
LeanderCretegny <leander.cretegny@epfl.ch>
2026-03-29 19:54:18 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #Defines utility var
-BLENDER_ARCHIVE=blender-4.5.8-linux-x64.tar.xz
+BLENDER_ARCHIVE=blender-4.5.8-linux-x64
 
 #Install blender 4.5.8 LTS (possible upgrade: install latest 4.5 version)
-curl -L -O "https://www.blender.org/download/lts/4-5/${BLENDER_ARCHIVE}" 
+curl -L -O "https://download.blender.org/release/Blender4.5/${BLENDER_ARCHIVE}.tar.xz" 
 
 #unzip blender
-tar xf ${BLENDER_ARCHIVE} && mv ${BLENDER_ARCHIVE} blender
+tar xf ${BLENDER_ARCHIVE}.tar.xz && mv ${BLENDER_ARCHIVE} blender
 
 # Start blender
 ./blender/blender -h
